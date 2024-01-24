000fc3bd462aa88b4c46c0059300dc6e629e6165
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2024-01-25 03:20:17 +0600

    parse request paths

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer(function (req, res) {
+  // Get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path from the url
+  const path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Send response
+  res.end('Hello world\n');
+
+  // log what path the user was asking for
+  console.log(`Request received on path ${trimmedPath}`);
+});
+
+server.listen(3000, function () {
+  console.log('The server is listening on port 3000 now');
+});
