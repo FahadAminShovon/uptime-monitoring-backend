573806bd323c9613c17b560f41d28072382d33de
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2024-01-25 20:42:40 +0600

    parse request payload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 const server = http.createServer(function (req, res) {
   // Get the url and parse it
@@ -11,21 +12,31 @@ const server = http.createServer(function (req, res) {
 
   const queryStringObject = parsedUrl.query;
 
-  const headers = req.headers;
-
   // Get the http method
   const method = req.method.toLowerCase();
 
-  // Send response
-  res.end('Hello world\n');
+  const headers = req.headers;
 
-  // log what path the user was asking for
-  console.log(
-    `Request received on path: ${trimmedPath} with method: ${method} with parameters`,
-    JSON.stringify(queryStringObject)
-  );
+  // Get payloads if there is any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
 
-  console.log('Request received with these headers', headers);
+  req.on('data', function (data) {
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', function () {
+    // Send response
+    res.end('Hello world\n');
+
+    // log what path the user was asking for
+    console.log(
+      `Request received on path: ${trimmedPath} with method: ${method} with parameters 
+      ${JSON.stringify(queryStringObject)} with payload ${buffer}`
+    );
+
+    console.log('Request received with these headers', headers);
+  });
 });
 
 server.listen(3000, function () {
