7c5dcfc1eeedb3039839bbafd0b0098771298dbc
FahadAminShovon <shovonfahad991@gmail.com>
2024-08-18 00:45:54 +0600

    add man command

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Dependencies
 const server = require('./lib/server');
 const workers = require('./lib/workers');
+const cli = require('./lib/cli');
 
 // Declare the app
 const app = {};
@@ -12,6 +13,12 @@ app.init = function () {
   server.init();
   // Start the workers
   workers.init();
+
+  // Start the CLI, but make sure it starts last
+
+  setTimeout(function () {
+    cli.init();
+  }, 50);
 };
 
 // Execute
